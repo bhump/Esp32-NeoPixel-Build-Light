11de4b763550461aaca1c7847ce67d9f40642cc7
Brent Humphrey <brent@snapspot.io>
2020-05-30 22:57:55 -0400

    Added Static IP.

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,22 @@
 int maxBrightness = 255;
 int minBrightness = 80;
 
-const char* host = "esp32";
+const char *host = "esp32";
 const char *ssid = "MoBhaile-2.4";
 const char *password = "beerisgood";
 const String accessToken = "cbt2e3vsrcrxrabdqqemzlhgpdh7ed5sonaivnfcnqhvqkvsl7ba";
 const String azureUrl = "https://dev.azure.com/BISSELL/BISSELL_IoT/_apis/build/builds?definitions=147&$top=1&api-version=5.1";
 const char *baseUrl = "https://dev.azure.com";
 const int httpsPort = 443;
+
+IPAddress local_IP(192, 168, 1, 231);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
+
+unsigned long previousMillis = 0; // will store last time LED was updated
+const long interval = 1000;       // interval at which to blink (milliseconds)
 
 WebServer server(80);
 
@@ -35,91 +44,77 @@ bool inProgress = false;
 String lastStatus;
 HTTPClient http;
 
-/*
- * Login page
- */
+/* Style */
+String style =
+    "<style>#file-input,input{width:100%;height:44px;border-radius:4px;margin:10px auto;font-size:15px}"
+    "input{background:#f1f1f1;border:0;padding:0 15px}body{background:#3498db;font-family:sans-serif;font-size:14px;color:#777}"
+    "#file-input{padding:0;border:1px solid #ddd;line-height:44px;text-align:left;display:block;cursor:pointer}"
+    "#bar,#prgbar{background-color:#f1f1f1;border-radius:10px}#bar{background-color:#3498db;width:0%;height:10px}"
+    "form{background:#fff;max-width:258px;margin:75px auto;padding:30px;border-radius:5px;text-align:center}"
+    ".btn{background:#3498db;color:#fff;cursor:pointer}</style>";
 
-const char* loginIndex = 
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<td>Username:</td>"
-        "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
+/* Login page */
+String loginIndex =
+    "<form name=loginForm>"
+    "<h1>BISSELL Login</h1>"
+    "<input name=userid placeholder='User ID'> "
+    "<input name=pwd placeholder=Password type=Password> "
+    "<input type=submit onclick=check(this.form) class=btn value=Login></form>"
+    "<script>"
+    "function check(form) {"
     "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
+    "{window.open('/serverIndex')}"
     "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
+    "{alert('Error Password or Username')}"
     "}"
+    "</script>" +
+    style;
+
+/* Server Index Page */
+String serverIndex =
+    "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+    "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+    "<input type='file' name='update' id='file' onchange='sub(this)' style=display:none>"
+    "<label id='file-input' for='file'>   Choose file...</label>"
+    "<input type='submit' class=btn value='Update'>"
+    "<br><br>"
+    "<div id='prg'></div>"
+    "<br><div id='prgbar'><div id='bar'></div></div><br></form>"
+    "<script>"
+    "function sub(obj){"
+    "var fileName = obj.value.split('\\\\');"
+    "document.getElementById('file-input').innerHTML = '   '+ fileName[fileName.length-1];"
+    "};"
+    "$('form').submit(function(e){"
+    "e.preventDefault();"
+    "var form = $('#upload_form')[0];"
+    "var data = new FormData(form);"
+    "$.ajax({"
+    "url: '/update',"
+    "type: 'POST',"
+    "data: data,"
+    "contentType: false,"
+    "processData:false,"
+    "xhr: function() {"
+    "var xhr = new window.XMLHttpRequest();"
+    "xhr.upload.addEventListener('progress', function(evt) {"
+    "if (evt.lengthComputable) {"
+    "var per = evt.loaded / evt.total;"
+    "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+    "$('#bar').css('width',Math.round(per*100) + '%');"
     "}"
-"</script>";
- 
-/*
- * Server Index Page
- */
- 
-const char* serverIndex = 
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-   "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-    "</form>"
- "<div id='prg'>progress: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')" 
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>";
+    "}, false);"
+    "return xhr;"
+    "},"
+    "success:function(d, s) {"
+    "console.log('success!') "
+    "},"
+    "error: function (a, b, c) {"
+    "}"
+    "});"
+    "});"
+    "</script>" +
+    style;
 
 void setColor(String color)
 {
@@ -157,12 +152,32 @@ void setColor(String color)
   strip.show();
 }
 
+void blinkUpdate()
+{
+  bool isOn = false;
+
+  if (isOn == true)
+  {
+    isOn = false;
+    setColor("blue");
+  }
+  else
+  {
+    isOn = true;
+    setColor("none");
+  }
+}
+
 bool initializeWifi()
 {
   if (WiFi.status() == WL_CONNECTED)
   {
     Serial.println("WiFi already connected");
     return true;
+  }
+
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
   }
 
   Serial.print("Connecting to ");
@@ -201,10 +216,12 @@ bool initializeWifi()
 
 void initializeWebServer()
 {
-    /*use mdns for host name resolution*/
-  if (!MDNS.begin(host)) { //http://esp32.local
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host))
+  { //http://esp32.local
     Serial.println("Error setting up MDNS responder!");
-    while (1) {
+    while (1)
+    {
       delay(1000);
     }
   }
@@ -219,30 +236,39 @@ void initializeWebServer()
     server.send(200, "text/html", serverIndex);
   });
   /*handling uploading firmware file */
-  server.on("/update", HTTP_POST, []() {
+  server.on(
+      "/update", HTTP_POST, []() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
-  }, []() {
+    ESP.restart(); }, []() {
     HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
+    if (upload.status == UPLOAD_FILE_START) 
+    {
       Serial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN))
+       { //start with max available size
         Update.printError(Serial);
       }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
+    } 
+    else if (upload.status == UPLOAD_FILE_WRITE)
+     {
       /* flashing firmware to ESP*/
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) 
+      {
         Update.printError(Serial);
       }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) { //true to set the size to the current progress
+    } 
+    else if (upload.status == UPLOAD_FILE_END) 
+    {
+      if (Update.end(true)) 
+      { //true to set the size to the current progress
         Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
+      } 
+      else 
+      {
         Update.printError(Serial);
       }
-    }
-  });
+    } });
   server.begin();
 }
 
@@ -256,7 +282,7 @@ void setup()
 
   isConnected = initializeWifi();
 
-  if(isConnected)
+  if (isConnected)
   {
     initializeWebServer();
   }
@@ -287,7 +313,7 @@ void getStatus()
     int httpCode = http.GET();
 
     if (httpCode > 0)
-    { //Check for the returning code
+    {
 
       String payload = http.getString();
       Serial.println(httpCode);
@@ -372,9 +398,10 @@ void TestColors()
 uint8_t reconnects = 0;
 void loop()
 {
+  server.handleClient();
+
   if (initializeWifi())
   {
-    server.handleClient();
     Serial.println("Getting Status");
     getStatus();
   }
@@ -390,4 +417,18 @@ void loop()
 
     delay(5000);
   }
+
+  // unsigned long currentMillis = millis();
+  // if (currentMillis - previousMillis >= interval)
+  // {
+
+  //   // // save the last time you blinked the LED
+  //   previousMillis = currentMillis;
+  //   //blinkUpdate();
+
+  //   // if the LED is off turn it on and vice-versa:
+  //   ledState = not(ledState);
+  //   // set the LED with the ledState of the variable:
+  //   digitalWrite(led, ledState);
+  // }
 }
